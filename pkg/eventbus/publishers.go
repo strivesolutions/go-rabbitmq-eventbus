d8cc3a315dd7314e2efe4ebf975116ed905eee4a
Michael Dryden <mjdryden@gmail.com>
2022-11-02 15:40:27 -0500

    Add routing key to publishjson

--- pkg/eventbus/publishers.go
+++ pkg/eventbus/publishers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rabbitmq/amqp091-go"
 )
 
-func PublishJson(exchangeName string, payload interface{}, publishTimeout time.Duration) error {
+func PublishJson(exchangeName, routingKey string, payload interface{}, publishTimeout time.Duration) error {
 	body, err := json.Marshal(payload)
 	if err != nil {
 		return err
@@ -17,7 +17,7 @@ func PublishJson(exchangeName string, payload interface{}, publishTimeout time.D
 	ctx, cancel := context.WithTimeout(context.Background(), publishTimeout)
 	defer cancel()
 
-	return currentChannel.PublishWithContext(ctx, exchangeName, "", false, false, amqp091.Publishing{
+	return currentChannel.PublishWithContext(ctx, exchangeName, routingKey, false, false, amqp091.Publishing{
 		ContentType: "application/json",
 		Body:        body,
 	})
