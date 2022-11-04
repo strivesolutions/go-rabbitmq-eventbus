e20d172caacbf46523f21bbb8bf7d6a4f6520afb
Michael Dryden <mjdryden@gmail.com>
2022-11-04 10:17:39 -0500

    Expose connection errors

--- pkg/eventbus/connection.go
+++ pkg/eventbus/connection.go
@@ -2,13 +2,18 @@ package eventbus
 
 import (
 	"errors"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type OnConnectedFunc func()
+type LogFunc func(msg string)
+
 var currentConnectionString string
 var currentConnection *amqp.Connection
 var currentChannel *amqp.Channel
+var connectionError chan error
 
 func ensureConnected() error {
 	if currentConnection == nil || currentChannel == nil {
@@ -16,6 +21,40 @@ func ensureConnected() error {
 	}
 	return nil
 }
+
+func statusString(isClosed bool) string {
+	if isClosed {
+		return "closed"
+	} else {
+		return "open"
+	}
+}
+
+func Status() string {
+	if currentConnection == nil || currentChannel == nil {
+		return "not configured"
+	}
+
+	return fmt.Sprintf("Connection: %s\t Channel: %s", statusString(currentConnection.IsClosed()), statusString(currentChannel.IsClosed()))
+}
+
+// func establishConnection(connectionString string, retryTime time.Duration, infoLog, warnLog, errorLog LogFunc) {
+// 	infoLog("Estabilishing connection to event bus")
+// 	err := Connect(connectionString)
+
+// 	if err != nil {
+// 		warnLog(fmt.Sprintf("Failed to connect to event bus, will try again in %s", retryTime))
+// 		errorLog(fmt.Sprint(err))
+// 		time.Sleep(retryTime)
+// 		establishConnection(connectionString, retryTime, infoLog, warnLog, errorLog)
+// 	}
+// }
+
+// func Connect2(connectionString string, retryTime time.Duration, infoLog, warnLog, errorLog LogFunc, onConnected OnConnectedFunc) {
+// 	establishConnection(connectionString, retryTime, infoLog, warnLog, errorLog)
+// 	onConnected()
+// 	return nil
+// }
 
 func Connect(connectionString string) error {
 	if currentConnection != nil {
@@ -33,12 +72,18 @@ func Connect(connectionString string) error {
 		return err
 	}
 
+	currentConnection = connection
+
+	go func() {
+		<-currentConnection.NotifyClose(make(chan *amqp.Error))
+		connectionError <- errors.New(("connection closed"))
+	}()
+
 	channel, err := connection.Channel()
 	if err != nil {
 		return err
 	}
 
-	currentConnection = connection
 	currentChannel = channel
 
 	return nil
